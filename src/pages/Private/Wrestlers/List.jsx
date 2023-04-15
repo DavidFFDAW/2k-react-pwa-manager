4aea420b9a9f2340b932d81d3e20fb39dece8011
DavidFFDAW <davidferflo2@gmail.com>
2023-04-16 00:28:07 +0200

    feat: spinner

--- src/pages/Private/Wrestlers/List.jsx
+++ src/pages/Private/Wrestlers/List.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
 import useWrestler from './hooks/useWrestler';
+import Spinner from '~/components/Spinner/Spinner';
 
 export default function List() {
     const { wrestlerList } = useWrestler();
+
+    if (wrestlerList.loading) {
+        return <Spinner />;
+    }
 
     return (
         <div>
             <h1>List</h1>
 
-            {!wrestlerList.loading ? (
-                <ul>
-                    {wrestlerList.list.slice(0, 10).map(wrestler => (
-                        <li key={wrestler.id}>{wrestler.name}</li>
-                    ))}
-                </ul>
-            ) : (
-                <h1>Loading...</h1>
-            )}
+            <ul>
+                {wrestlerList.list.slice(0, 10).map(wrestler => (
+                    <li key={wrestler.id}>{wrestler.name}</li>
+                ))}
+            </ul>
         </div>
     );
 }
