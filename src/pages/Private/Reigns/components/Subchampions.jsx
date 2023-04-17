186471982d9db996cb12b6f6bc04a18707e32078
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-04-17 14:23:43 +0200

    fix: image only when no team and team name

--- src/pages/Private/Reigns/components/Subchampions.jsx
+++ src/pages/Private/Reigns/components/Subchampions.jsx
@@ -6,18 +6,19 @@ export default function Subchampions({ subchampions }) {
         <ul className='w1 flex center column al-center gap-small' style={{ listStyle: 'none' }}>
             {subchampions.map((champ) => {
                 const wrestler = champ.wrestler;
+                const wrestlerOrTeamName = champ.championship.tag ? wrestler.team.name : wrestler.name;
                 // const key = `${champ.title_reign_id}-${wrestler.id}-${champ.championship.id}`;
 
                 return (
                     <li key={champ.title_reign_id} className='w1 bounce'>
                         <div className="w1 wrestler-center flex center al-center">
                             <div className="w1 flex start al-start gap boxed">
-                                <div className="wrestler-card__image flex start al-center column">
+                                {champ.championship.tag && <div className="wrestler-card__image flex start al-center column">
                                     <img style={{ maxWidth: '65%' }} src={wrestler.image} alt={wrestler.name} />
-                                </div>
+                                </div>}
 
                                 <div className="w1 wrestler-card__info">
-                                    <h3 className="wrestler-card__name">{wrestler.name}</h3>
+                                    <h3 className="wrestler-card__name">{wrestlerOrTeamName}</h3>
                                     <p>{champ.days} días</p>
                                     {Boolean(champ.current) && <p>Actual</p>}
                                 </div>
@@ -27,7 +28,7 @@ export default function Subchampions({ subchampions }) {
                     </li>
                 )
             })}
-        </ul>
+        </ul >
 
     )
 }
