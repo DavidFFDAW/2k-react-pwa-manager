e9edc2710af41b0ccbd81a163a3c548872a3a616
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-04-25 11:04:54 +0200

    chore: imagePreview support for sizes

--- src/pages/Private/Wrestlers/components/ImagePreview.jsx
+++ src/pages/Private/Wrestlers/components/ImagePreview.jsx
@@ -1,4 +1,8 @@
-export default function ImagePreview({ image, name }) {
+export default function ImagePreview({ image, name, maxW, maxH, px = false }) {
+    const maxWidth = Number(maxW) ? Number(maxW) : 30;
+    const maxHeight = Number(maxH) ? Number(maxH) : 30;
+    const convertToPx = max => px ? max : `${max}%`;
+
     const finalImage = image || '/noimage.jpg';
     const alt = name ? `${name} icon preview` : 'wrestler icon preview';
     const title = name ? `${name} icon preview` : 'wrestler icon preview';
@@ -10,5 +14,11 @@ export default function ImagePreview({ image, name }) {
         target.alt = 'No image';
     };
 
-    return <img style={{ maxWidth: '30%' }} className="rounded-preview-icon" src={finalImage} alt={alt} title={title} onError={errorHandler} />;
+    const style = {
+        maxWidth: convertToPx(maxWidth),
+        maxHeight: convertToPx(maxHeight),
+        height: convertToPx(maxHeight),
+    }
+
+    return <img style={style} className="rounded-preview-icon" src={finalImage} alt={alt} title={title} onError={errorHandler} />;
 }
