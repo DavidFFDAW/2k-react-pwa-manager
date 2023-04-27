f948c3b9913f61f7162b2ad06c88493592c3f766
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-04-27 13:23:49 +0200

    chore: global hooks

---
+++ src/hooks/useGlobalWrestlers.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import useAbortRequest from '~/hooks/useAbortRequest';
+import useHttp from '~/hooks/useHttp'
+
+export default function useGlobalWrestlers( endpoint ) {
+      const http = useHttp();
+      const aborter = useAbortRequest();
+      const [wrestlers, setWrestlers] = React.useState([])
+
+      aborter.requestWithAbort(_ => http.APIGet(`wrestlers/${endpoint}`).then(setWrestlers))
+      
+      return {
+            wrestlers
+      }
+}
