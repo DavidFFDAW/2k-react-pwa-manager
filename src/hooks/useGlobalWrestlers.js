a077040d5c1cb49d3c8439df7228a9d1e3ff48b9
DavidFFDAW <davidferflo2@gmail.com>
2023-04-29 19:06:29 +0200

    chore: alert param http requests

--- src/hooks/useGlobalWrestlers.js
+++ src/hooks/useGlobalWrestlers.js
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from 'react';
 import useAbortRequest from '~/hooks/useAbortRequest';
-import useHttp from '~/hooks/useHttp'
+import useHttp from '~/hooks/useHttp';
 
-export default function useGlobalWrestlers( endpoint ) {
-      const http = useHttp();
-      const aborter = useAbortRequest();
-      const [wrestlers, setWrestlers] = React.useState([])
+export default function useGlobalWrestlers(endpoint) {
+    const http = useHttp();
+    const aborter = useAbortRequest();
+    const [wrestlers, setWrestlers] = React.useState([]);
 
-      aborter.requestWithAbort(_ => http.APIGet(`wrestlers/${endpoint}`).then(setWrestlers))
-      
-      return {
-            wrestlers
-      }
+    aborter.requestWithAbort(_ => http.APIGet(`wrestlers/${endpoint}`).then(setWrestlers));
+
+    return {
+        wrestlers,
+    };
 }
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -3,31 +3,35 @@ import { AppConfig } from '~/AppConfig';
 import HttpService from '~/services/http.service';
 
 export default function useHttp() {
-    const processResponse = response => {
+    const processResponse = (response, notification = false) => {
         if (!response.ok) {
             const message = response.content.message || 'Something went wrong';
             enqueueSnackbar(message, { variant: 'error' });
             throw new Error(message);
         }
 
+        if (notification) {
+            const message = notification.success || 'Exito al guardar';
+            enqueueSnackbar(message, { variant: 'success' });
+        }
+
         return response.content;
     };
 
     const API = AppConfig.API_BASE_URL;
-
-    const get = url => HttpService.get(url).then(processResponse);
-    const post = (url, data) => HttpService.post(url, data).then(processResponse);
-    const put = (url, data) => HttpService.put(url, data).then(processResponse);
-    const deleteReq = url => HttpService.delete(url).then(processResponse);
+    const get = (url, notification = false) => HttpService.get(url).then(response => processResponse(response, notification));
+    const post = (url, data, notification = false) => HttpService.post(url, data).then(response => processResponse(response, notification));
+    const put = (url, data, notification = false) => HttpService.put(url, data).then(response => processResponse(response, notification));
+    const deleteReq = (url, notification = false) => HttpService.delete(url).then(response => processResponse(response, notification));
 
     return {
         get,
         post,
         put,
         deleteReq,
-        APIGet: endpoint => get(`${API}${endpoint}`),
-        APIPost: (endpoint, data) => post(`${API}${endpoint}`, data),
-        APIPut: (endpoint, data) => put(`${API}${endpoint}`, data),
-        APIDelete: endpoint => deleteReq(`${API}${endpoint}`),
+        APIGet: (endpoint, notif) => get(`${API}${endpoint}`, notif),
+        APIPost: (endpoint, data, notif) => post(`${API}${endpoint}`, data, notif),
+        APIPut: (endpoint, data, notif) => put(`${API}${endpoint}`, data, notif),
+        APIDelete: (endpoint, notif) => deleteReq(`${API}${endpoint}`, notif),
     };
 }
