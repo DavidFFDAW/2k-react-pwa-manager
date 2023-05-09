9864df12057407aaf175cbb5a31f3762de8a08ca
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-05-09 09:39:09 +0200

    chore: create team route

--- src/pages/Private/Reigns/ReignsRouting.jsx
+++ src/pages/Private/Reigns/ReignsRouting.jsx
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom';
 import RoutesWithNotFound from '~/components/RoutesWithNotFound';
 
 const ChampionsList = React.lazy(() => import('./pages/List'));
+const ChampionsCreate = React.lazy(() => import('./pages/Create'));
 
 export default function WrestlersRouting() {
     return (
         <RoutesWithNotFound>
             <Route path={'/'} element={<ChampionsList />}></Route>
+            <Route path={'/create/team'} element={<ChampionsCreate />}></Route>
         </RoutesWithNotFound>
     );
 }
