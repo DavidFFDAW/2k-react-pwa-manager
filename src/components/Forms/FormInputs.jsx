fb7fc407edb431b9e71dcdf4c99e31a18af55d46
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-05-10 09:38:52 +0200

    feat: input custom change callback

--- src/components/Forms/FormInputs.jsx
+++ src/components/Forms/FormInputs.jsx
@@ -1,16 +1,19 @@
-export default function UpsertInput({ type, max, label, property, formState, setFormState, required = false }) {
+export default function UpsertInput({ type, max, label, property, formState, setFormState, onChangeCallback, required = false }) {
+    const value = formState[`${property}`] || '';
+    const defaultChange = ev => setFormState({ ...formState, [property]: ev.target.value });
+    const change = !setFormState && Boolean(onChangeCallback) ? onChangeCallback : defaultChange;
+
     return (
         <div className="w1 flex column gap-5">
             <label className="label">{label}</label>
             <input
                 className="w1"
-                max={max}
-                maxLength={max}
-                type={type}
+                maxLength={max || 100}
+                type={type || 'text'}
                 name={property}
                 required={required}
-                value={formState[`${property}`]}
-                onChange={ev => setFormState({ ...formState, [property]: ev.target.value })}
+                value={value}
+                onChange={change}
             />
         </div>
     );
