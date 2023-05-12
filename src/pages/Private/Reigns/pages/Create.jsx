8d1f47bb18368eaccf3f490421baff3d4f1c2239
DavidFFDAW <davidferflo2@gmail.com>
2023-05-12 15:20:31 +0200

    fix: git error

--- src/pages/Private/Reigns/pages/Create.jsx
+++ src/pages/Private/Reigns/pages/Create.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import useCreateReign, { ITEMS } from '../hooks/useCreateReign'
+import React from 'react';
+import useCreateReign, { ITEMS } from '../hooks/useCreateReign';
 import { ComponentSpinner } from '~/components/Spinner/Spinner';
 import CustomSelect from '~/components/CustomSelect/CustomSelect';
 import { Boxed } from '~/components/Box/Boxed';
@@ -9,21 +9,31 @@ import UpsertInput, { UpsertToggle } from '~/components/Forms/FormInputs';
 export default function Create() {
     const { createDatas, form, setFormState, getItemID, submitForm } = useCreateReign();
 
-    const CustomSelectChampions = createDatas.championships.length > 0 ? <CustomSelect
-        nameProp={'name'}
-        imageProp={'image'}
-        list={createDatas.championships}
-        getIdCallback={id => getItemID(id, ITEMS.CHAMPIONSHIP)}
-    /> : <ComponentSpinner />
+    const CustomSelectChampions =
+        createDatas.championships.length > 0 ? (
+            <CustomSelect
+                nameProp={'name'}
+                imageProp={'image'}
+                list={createDatas.championships}
+                getIdCallback={id => getItemID(id, ITEMS.CHAMPIONSHIP)}
+            />
+        ) : (
+            <ComponentSpinner />
+        );
 
     const isTagTeam = Boolean(createDatas.selectedChampionship.tag);
 
-    const CustomSelectWrestlers = createDatas.wrestlers.length > 0 ? <CustomSelect
-        nameProp={'name'}
-        imageProp={'image'}
-        list={createDatas.wrestlers}
-        getIdCallback={id => getItemID(id, ITEMS.WRESTLER)}
-    /> : <ComponentSpinner />
+    const CustomSelectWrestlers =
+        createDatas.wrestlers.length > 0 ? (
+            <CustomSelect
+                nameProp={'name'}
+                imageProp={'image'}
+                list={createDatas.wrestlers}
+                getIdCallback={id => getItemID(id, ITEMS.WRESTLER)}
+            />
+        ) : (
+            <ComponentSpinner />
+        );
 
     console.log({
         createDatas,
@@ -32,33 +42,34 @@ export default function Create() {
 
     return (
         <>
-            <form
-                method="POST"
-                className="flex center al-center column gap wrestler-upsert-form"
-                onSubmit={submitForm}
-            >
+            <form method="POST" className="flex center al-center column gap wrestler-upsert-form" onSubmit={submitForm}>
                 <Boxed title={'Título'}>
-                    <div className="w1 flex column al-start gap-small">
-                        {CustomSelectChampions}
-                    </div>
-                    <UpsertToggle toggleCallback={_ => setFormState(p => ({ ...p, isCurrent: !p.isCurrent }))} label={'Actual'} />
+                    <div className="w1 flex column al-start gap-small">{CustomSelectChampions}</div>
                 </Boxed>
 
-                {!isTagTeam ? <Boxed title={'Luchador'}>
-                    <div className="w1 flex column al-start gap-small">
-                        {CustomSelectWrestlers}
-                    </div>
-                </Boxed> : null}
+                {!isTagTeam ? (
+                    <Boxed title={'Luchador'}>
+                        <div className="w1 flex column al-start gap-small">{CustomSelectWrestlers}</div>
+                    </Boxed>
+                ) : null}
 
-                {isTagTeam ? <TeamPanel
-                    createDatas={createDatas}
-                    form={form}
-                    setFormState={setFormState}
-                    getTeamID={id => getItemID(id, ITEMS.TEAM)}
-                /> : null}
+                {isTagTeam ? (
+                    <TeamPanel
+                        createDatas={createDatas}
+                        form={form}
+                        setFormState={setFormState}
+                        getTeamID={id => getItemID(id, ITEMS.TEAM)}
+                    />
+                ) : null}
 
                 <Boxed title={'Datos del reinado'}>
                     <div className="w1 flex column al-start gap">
+                        <div>
+                            <UpsertToggle
+                                toggleCallback={_ => setFormState(p => ({ ...p, isCurrent: !p.isCurrent }))}
+                                label={'Actual'}
+                            />
+                        </div>
 
                         <UpsertInput
                             type={'date'}
@@ -69,29 +80,25 @@ export default function Create() {
                             setFormState={setFormState}
                         />
 
-                        {form.isCurrent ? null : <UpsertInput
-                            type={'date'}
-                            name={'end'}
-                            label={'Fecha Fin'}
-                            property={'end'}
-                            formState={form}
-                            setFormState={setFormState}
-                        />}
+                        {form.isCurrent ? null : (
+                            <UpsertInput
+                                type={'date'}
+                                name={'end'}
+                                label={'Fecha Fin'}
+                                property={'end'}
+                                formState={form}
+                                setFormState={setFormState}
+                            />
+                        )}
                     </div>
                 </Boxed>
 
-
                 <div className="w90 flex end al-center gap">
-                    <button
-                        type="submit"
-                        className="cta"
-                        disabled={createDatas.loading}
-                    >
+                    <button type="submit" className="cta" disabled={createDatas.loading}>
                         Crear
                     </button>
                 </div>
             </form>
-
         </>
-    )
+    );
 }
