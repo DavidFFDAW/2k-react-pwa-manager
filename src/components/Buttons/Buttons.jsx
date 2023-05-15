00e1a868363843caa5e839817586a9fb46dbcb55
DavidFFDAW <davidfernandez@madeinmedia.es>
2023-05-15 15:04:22 +0200

    chore: disabled Button components

--- src/components/Buttons/Buttons.jsx
+++ src/components/Buttons/Buttons.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
-export default function Button({ text, type, className, onClick }) {
+export default function Button({ text, type, className, onClick, disabled = false }) {
     const buttonType = type || 'button';
     const buttonClass = className ? `btn button ${className}` : 'btn button';
 
     return (
-        <button className={buttonClass} role="button" type={buttonType} onClick={onClick}>
+        <button disabled={disabled} className={buttonClass} role="button" type={buttonType} onClick={onClick}>
             {text}
         </button>
     );
 }
 
-export function ButtonCTA({ text, type, onClick }) {
-    return <Button text={text} type={type} onClick={onClick} className={'cta'} />;
+export function ButtonCTA({ text, type, onClick, disabled = false }) {
+    return <Button disabled={disabled} text={text} type={type} onClick={onClick} className={'cta'} />;
 }
 
-export function InfoButton({ text, type, onClick }) {
-    return <Button text={text} type={type} onClick={onClick} className={'action-button info-action'} />;
+export function InfoButton({ text, type, onClick, disabled = false }) {
+    return <Button disabled={disabled} text={text} type={type} onClick={onClick} className={'action-button info-action'} />;
 }
 
-export function DangerButton({ text, type, onClick }) {
-    return <Button text={text} type={type} onClick={onClick} className={'action-button danger-action'} />;
+export function DangerButton({ text, type, onClick, disabled = false }) {
+    return <Button disabled={disabled} text={text} type={type} onClick={onClick} className={'action-button danger-action'} />;
 }
 
-export function GreyButton({ text, type, onClick }) {
-    return <Button text={text} type={type} onClick={onClick} className={'action-button default-action'} />;
+export function GreyButton({ text, type, onClick, disabled = false }) {
+    return <Button disabled={disabled} text={text} type={type} onClick={onClick} className={'action-button default-action'} />;
 }
