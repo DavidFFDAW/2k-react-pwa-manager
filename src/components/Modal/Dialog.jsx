cbb38f6eb72ddd94600a920656a106ac87bf589d
DavidFFDAW <davidferflo2@gmail.com>
2023-06-17 19:27:00 +0200

    feat: dialog with custom footer component

--- src/components/Modal/Dialog.jsx
+++ src/components/Modal/Dialog.jsx
@@ -13,10 +13,26 @@ export default function Dialog({ visible, toggleVisibility, children }) {
                         </button>
                     </header>
                     <section className="dialog-content">{children}</section>
+                </dialog>
+            </NullableLoading>
+        </>
+    );
+}
 
-                    {/* <footer className="flex end dialog-footer">
-                        <ButtonSecondary text={'Cerrar'} onClick={toggleVisibility} />
-                    </footer> */}
+export function DialogWithFooter({ visible, toggleVisibility, children, footer }) {
+    return (
+        <>
+            <NullableLoading condition={visible}>
+                <div className="background-action-block"></div>
+                <dialog open={visible} className="dialog-modal">
+                    <header className="dialog-header">
+                        <button className="dialog-close-button" type="button" onClick={toggleVisibility}>
+                            &times;
+                        </button>
+                    </header>
+                    <section className="dialog-content">{children}</section>
+
+                    <footer className="flex end dialog-footer">{footer}</footer>
                 </dialog>
             </NullableLoading>
         </>
