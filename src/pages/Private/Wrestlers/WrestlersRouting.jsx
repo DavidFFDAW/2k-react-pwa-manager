d5dc42b3ea8a0e59bf0ab47a2c40cc8ef03fb7d3
DavidFFDAW <davidferflo2@gmail.com>
2023-06-18 18:41:53 +0200

    chore: no redirection on wrestlers

--- src/pages/Private/Wrestlers/WrestlersRouting.jsx
+++ src/pages/Private/Wrestlers/WrestlersRouting.jsx
@@ -9,9 +9,10 @@ const UpsertPage = React.lazy(() => import('./Upsert'));
 export default function WrestlersRouting() {
     return (
         <RoutesWithNotFound>
-            <Route path="/" element={<Navigate to={'/admin/wrestlers/active/page/1'} replace />}></Route>
-            <Route path={'/all/page/:page'} element={<WrestlersList endpoint={'all'} />}></Route>
-            <Route path={'/active/page/:page'} element={<WrestlersList endpoint={'active'} />}></Route>
+            {/* <Route path="/" element={<Navigate to={'/admin/wrestlers/active/page/1'} replace />}></Route> */}
+            <Route path="/" element={<WrestlersList />}></Route>
+            <Route path={'/page/:page'} element={<WrestlersList />}></Route>
+            {/* <Route path={'/active/page/:page'} element={<WrestlersList endpoint={'active'} />}></Route> */}
             <Route path={'create/new'} element={<UpsertPage type={TYPES.CREATE} />}></Route>
             <Route path={'update/:id'} element={<UpsertPage type={TYPES.UPDATE} />}></Route>
         </RoutesWithNotFound>
