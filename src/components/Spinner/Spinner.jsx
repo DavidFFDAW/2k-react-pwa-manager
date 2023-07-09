24357d036c22afae55888a0115a4b23b32a471a3
DavidFFDAW <davidferflo2@gmail.com>
2023-07-10 01:03:11 +0200

    feat: DarkSpinner component

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -15,11 +15,21 @@ export default function Spinner() {
 }
 
 export function ComponentSpinner({ className }) {
-    const css = className ? `w1 flex center component-spinner-ch4478asdw ${className}` : 'w1 flex center component-spinner-ch4478asdw';
+    const css = className
+        ? `w1 flex center al-center component-spinner-ch4478asdw ${className}`
+        : 'w1 flex center al-center component-spinner-ch4478asdw';
 
     return (
         <div className={css}>
             <div className="spinner small"></div>
+        </div>
+    );
+}
+
+export function DarkSpinner({ className }) {
+    return (
+        <div className={`w1 flex center al-center component-spinner-ch4478asdw ${className}`}>
+            <div className="dark spinner small"></div>
         </div>
     );
 }
