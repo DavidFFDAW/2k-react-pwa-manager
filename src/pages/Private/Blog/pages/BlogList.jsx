2cfa1221090f0e940d7b4dde417b0272df0b0068
DavidFFDAW <davidferflo2@gmail.com>
2023-07-15 16:07:24 +0200

    chore: featured grouped actions popup

--- src/pages/Private/Blog/pages/BlogList.jsx
+++ src/pages/Private/Blog/pages/BlogList.jsx
@@ -4,17 +4,28 @@ import BlogCard from './components/BlogCard';
 import { makePrivateRoute } from '~/utilities/private.route.utility';
 import { PrivateRoutes } from '~/constants/routes';
 import CreateButton from '~/components/Buttons/CreateButton';
+import { NullableLoading } from '~/components/Loading/LoadingComponent';
 
 export default function BlogList() {
     const { blogPosts, functions } = useBlogList();
-
+    const styles = {
+        position: 'fixed',
+        bottom: 10,
+        left: 0,
+    };
+    const isGroupedAction = Object.values(blogPosts.checked).filter(value => Boolean(value)).length > 1;
     // if (blogPosts.loading) return <Spinner />;
 
     return (
         <>
-            <button type="button" onClick={functions.getCheckedBlogPosts}>
+            <NullableLoading condition={isGroupedAction}>
+                <div className="w1 flex center  animate__animated animate__fadeInUp" style={styles}>
+                    <div className="w1 boxed">asdadsad</div>
+                </div>
+            </NullableLoading>
+            {/* <button type="button" onClick={functions.getCheckedBlogPosts}>
                 Obtener seleccionados
-            </button>
+            </button> */}
             <div className="w1 blog-panel flex center">
                 <div className="w90 flex center gap column">
                     {blogPosts.list.map((post, index) => {
