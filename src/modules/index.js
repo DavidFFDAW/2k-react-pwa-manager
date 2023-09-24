aa4f75327e1edc4c9c197d3fed4fd57f6e212b4e
DavidFFDAW <davidferflo2@gmail.com>
2023-09-24 23:18:37 +0200

    feat: actions module

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,4 +1,7 @@
+export { default as GalleryModule } from './gallery/GalleryModule';
+export { default as Gallery } from './gallery/components/gallery/Gallery';
+export { default as useGallery } from './gallery/components/gallery/useGallery';
 export * from './gallery/components';
-export * from './gallery/hooks/useGalleryModule';
-export * from './gallery';
+export { default as GalleryContext } from './gallery/context/GalleryContext';
+export { default as useGalleryModule } from './gallery/hooks/useGalleryModule';
 export * from './gallery/services/gallery.api.service';
--- src/pages/Private/User/UserPage.jsx
+++ src/pages/Private/User/UserPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import Actions from '~/modules/actions/Actions';
 import GalleryModule from '~/modules/gallery/GalleryModule';
 
 function UserPage() {
-    return <GalleryModule />;
+    return (
+        <>
+            <GalleryModule />
+            <Actions.Container>
+                <Actions.Option toHref="/user/edit" text="Edit Profile" />
+            </Actions.Container>
+        </>
+    );
 }
 
 export default UserPage;
